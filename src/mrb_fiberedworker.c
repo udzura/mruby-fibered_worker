586c315f0ce1e9e3d44e26f646e67d1f88f2d4af
Uchio KONDO <udzura@pepabo.com>
2018-04-16 18:39:25 +0900

    Remove old logics

--- src/mrb_fiberedworker.c
+++ src/mrb_fiberedworker.c
@@ -23,19 +23,6 @@
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
-int mrb_signo_registered[64];
-int mrb_signo_signaled[64];
-
-static void mrb_fw_sighandler_func(int signo)
-{
-  mrb_signo_signaled[signo] = 1;
-}
-
-static int mrb_fw__is_registerd_signal(int signo)
-{
-  return mrb_signo_registered[signo];
-}
-
 static int signm2signo(const char *nm)
 {
   const struct signals *sigs;
@@ -161,30 +148,6 @@ static mrb_value mrb_fw_sigtimedwait(mrb_state *mrb, mrb_value self)
   }
 }
 
-static mrb_value mrb_fw_register_internal_handler(mrb_state *mrb, mrb_value self)
-{
-  mrb_value signo;
-  mrb_get_args(mrb, "o", &signo);
-  int signo_idx = mrb_to_signo(mrb, signo);
-  struct sigaction act;
-  if (mrb_fw__is_registerd_signal(signo_idx)) {
-    mrb_raise(mrb, E_RUNTIME_ERROR, "Signal already registered");
-  }
-  if (signo_idx > SIGRTMAX) {
-    mrb_raise(mrb, E_ARGUMENT_ERROR, "Signal NR must be less than SIGRTMAX");
-  }
-
-  act.sa_handler = mrb_fw_sighandler_func;
-  sigemptyset(&act.sa_mask);
-  act.sa_flags = SA_RESTART;
-
-  if (sigaction(signo_idx, &act, NULL) == -1) {
-    mrb_sys_fail(mrb, "sigaction");
-  }
-  mrb_signo_registered[signo_idx] = 1;
-  return mrb_fixnum_value(signo_idx);
-}
-
 static mrb_value mrb_fw_obj2signo(mrb_state *mrb, mrb_value self)
 {
   mrb_value signo;
@@ -192,46 +155,6 @@ static mrb_value mrb_fw_obj2signo(mrb_state *mrb, mrb_value self)
   int signo_idx = mrb_to_signo(mrb, signo);
 
   return mrb_fixnum_value((mrb_int)signo_idx);
-}
-
-static mrb_value mrb_fw_is_registered(mrb_state *mrb, mrb_value self)
-{
-  mrb_value signo;
-  mrb_get_args(mrb, "o", &signo);
-  int signo_idx = mrb_to_signo(mrb, signo);
-
-  return mrb_bool_value((mrb_bool)mrb_fw__is_registerd_signal(signo_idx));
-}
-
-static mrb_value mrb_fw_is_signaled(mrb_state *mrb, mrb_value self)
-{
-  mrb_value signo;
-  mrb_bool signaled = FALSE;
-  mrb_get_args(mrb, "o", &signo);
-  int signo_idx = mrb_to_signo(mrb, signo);
-  sigset_t mask, old_mask;
-  sigemptyset(&mask);
-  sigemptyset(&old_mask);
-  if (sigaddset(&mask, signo_idx) < 0) {
-    mrb_sys_fail(mrb, "sigaddset");
-  }
-  if (sigprocmask(SIG_BLOCK, &mask, &old_mask) < 0) {
-    mrb_sys_fail(mrb, "sigprocmask");
-  }
-
-  if (!mrb_signo_registered[signo_idx]) {
-    goto ensure_recovermask;
-  }
-  signaled = (mrb_bool)mrb_signo_signaled[signo_idx];
-  if (signaled) {
-    mrb_signo_signaled[signo_idx] = 0;
-  }
-
-ensure_recovermask:
-  if (sigprocmask(SIG_SETMASK, &old_mask, NULL) < 0) {
-    mrb_sys_fail(mrb, "sigprocmask: recovery");
-  }
-  return mrb_bool_value(signaled);
 }
 
 // FiberedWorker::Timer
@@ -371,19 +294,11 @@ static mrb_value mrb_timer_posix_is_running(mrb_state *mrb, mrb_value self)
 void mrb_mruby_fibered_worker_gem_init(mrb_state *mrb)
 {
   struct RClass *fiberedworker, *timer;
-  int i;
-  for (i = 0; i < SIGRTMAX; i++) {
-    mrb_signo_registered[i] = 0;
-    mrb_signo_signaled[i] = 0;
-  }
 
   fiberedworker = mrb_define_module(mrb, "FiberedWorker");
   mrb_define_module_function(mrb, fiberedworker, "sigprocmask", mrb_fw_sigprocmask, MRB_ARGS_REQ(1));
   mrb_define_module_function(mrb, fiberedworker, "sigtimedwait", mrb_fw_sigtimedwait, MRB_ARGS_REQ(2));
-  mrb_define_module_function(mrb, fiberedworker, "register_internal_handler", mrb_fw_register_internal_handler, MRB_ARGS_REQ(1));
-  mrb_define_module_function(mrb, fiberedworker, "registered?", mrb_fw_is_registered, MRB_ARGS_REQ(1));
   mrb_define_module_function(mrb, fiberedworker, "obj2signo", mrb_fw_obj2signo, MRB_ARGS_REQ(1));
-  mrb_define_module_function(mrb, fiberedworker, "signaled_nonblock?", mrb_fw_is_signaled, MRB_ARGS_REQ(1));
 
   mrb_define_const(mrb, fiberedworker, "SIGINT", mrb_fixnum_value(SIGINT));
   mrb_define_const(mrb, fiberedworker, "SIGHUP", mrb_fixnum_value(SIGHUP));
