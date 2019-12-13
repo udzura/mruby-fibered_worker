013c02e7fab0bf624f5282db83d3ba27db0913af
Uchio Kondo <udzura@udzura.jp>
2019-12-13 16:42:35 +0900

    Update samples

--- example/fd_watch.rb
+++ example/fd_watch.rb
@@ -8,10 +8,13 @@ pid = fork do
     puts "[#{Process.pid}] wait..."
     sleep 1
   end
-  w.write 'a'
-  loop do
+  data = 'mruby!'.split('')
+  while c = data.shift
     sleep 1
+    puts "Sending #{c.inspect}"
+    w.write c
   end
+  w.close
 end
 w.close
 loop.pid = pid
@@ -19,9 +22,12 @@ loop.register_timer(FiberedWorker::SIGRTMIN, 1000, 1000) do |signo|
   puts "[#{Process.pid}] wait..."
 end
 
-loop.register_fd(r.fileno) do |data|
-  puts "received: #{data}"
-  Process.kill :TERM, pid
+loop.register_fd(r.fileno, false) do |data|
+  puts "received: #{data}(#{data.chr.inspect})"
+  if data == '!'.ord
+    puts "Then finish"
+    Process.kill :TERM, pid
+  end
 end
 
 puts "Parent: [#{Process.pid}]"
